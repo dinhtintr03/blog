408c1131dc6750aaf54915c0006fe53832f70e1e
dinhtintr03 <learning.dinhtin@gmail.com>
2024-10-24 18:16:19 +0700

    base-html

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 // Routes
 router.get('', (req, res) => {
-    res.send("Hello World");
+    const locals = {
+        title: "Bay dat blog",
+        description: "Simple blog talk simple knowledge"
+    }
+    res.render('index', { locals });
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
 });
 
 module.exports = router;
